f601c286cadb0e4faeb1c997ab61f19af19fb527
Neetika23 <42495275+Neetika23@users.noreply.github.com>
2020-05-18 11:01:29 +0530

    Update train_test_split.py

--- Regression/train_test_split.py
+++ Regression/train_test_split.py
@@ -10,12 +10,12 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.3, rando
 reg_all = LinearRegression()
 
 # Fit the regressor to the training data
-reg_all.fit(X_train,y_train)
+reg_all.fit(X_train, y_train)
 
 # Predict on the test data: y_pred
 y_pred = reg_all.predict(X_test)
 
 # Compute and print R^2 and RMSE
 print("R^2: {}".format(reg_all.score(X_test, y_test)))
-rmse = np.sqrt(reg_all.score(X_test, y_test))
+rmse = np.sqrt(mean_squared_error(y_test, y_pred))
 print("Root Mean Squared Error: {}".format(rmse))
